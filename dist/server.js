563959405ab079bf86da5ff380b0f8a9fd2229fb
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2021-01-15 15:37:33 -0300

    Add start script e porta padrao

--- dist/server.js
+++ dist/server.js
@@ -4,6 +4,6 @@ var _app = _interopRequireDefault(require("./config/app"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-_app["default"].listen(8080, function () {
+_app["default"].listen(process.env.PORT || 8080, function () {
   console.log("Running...");
 });
